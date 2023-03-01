f4190b7749d0c2a16351750fcde37aa7a41d06dd
Ankita Binu <35870562+ankitabinu1998@users.noreply.github.com>
2023-03-01 16:10:50 -0500

    useState for like count

--- src/pages/main/post.tsx
+++ src/pages/main/post.tsx
@@ -1,21 +1,29 @@
 import {addDoc, collection} from 'firebase/firestore'
 import {auth, db} from '../../config/firebase'
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useState, useEffect } from 'react';
 
 interface LikeInt {
-    userWhoLikedId: string,
-    postId: string
+    data: {
+        userWhoLikedId: string,
+        postId: string
+    },
+    count: "number"
 }
 
 export const Post = (props:any) => {
     const [user] = useAuthState(auth);
+    let [likesCount, setLikesCount] = useState(props.post.count);
     const likesRef = collection(db,"likes");
     const onLikePost = async (data:LikeInt) => {
         await addDoc(likesRef,{
-            ...data
+            ...data.data
         });
+        let count = likesCount + 1;
+        setLikesCount(count);
     }
 
+    useEffect(()=>{},[likesCount])
 
     return (
         <div key={props.post.id} className="post-body">
@@ -23,16 +31,20 @@ export const Post = (props:any) => {
             <hr/>
             <p className="post-description">{props.post.description}</p>
             <p className="post-user-name">- {props.post.userName}</p>
-            {user && <button className="like-btn" disabled={user.uid !== props.post.userId} onClick={()=>
-                onLikePost(
-                    {
-                        userWhoLikedId: user?.uid,
-                        postId:props.post.id
-                    }
-                    )}>
-                &#128077;
-            </button>}
-            <p>{props.post.count}</p>
+            {user && <div className='like-btn-container'>
+                <button className="like-btn" disabled={user.uid === props.post.userId} onClick={()=>
+                    onLikePost(
+                        {
+                            data: {
+                                userWhoLikedId: user?.uid,
+                                postId:props.post.id},
+                            count: props.post.count
+                        }
+                        )}>
+                    &#128077;
+                </button>
+                <p>{likesCount <= 1 ? `${likesCount} Like` : `${likesCount} Likes`}</p>
+                </div>}
         </div>
     )
 }
